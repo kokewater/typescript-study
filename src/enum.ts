f1c81cdf0830b43806e8116a639ee9dd950f4144
ryo <ryokokeguchi@gmail.com>
2021-06-20 18:34:14 +0900

    add

---
+++ src/enum.ts
@@ -0,0 +1,33 @@
+export {};
+
+enum Months {
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    Jun,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+enum COLORS {
+    RED = '#FF0000',
+    WHITE = '#FFFFFF',
+    GREEN = '008000',
+    BLUR = '0000FF',
+    BLACK = '000000'
+}
+
+let green = COLORS.GREEN;
+console.log({ green });
+
+enum COLORS {
+    YELLOW = '#FFFF00'
+}
+
+COLORS.YELLOW;
